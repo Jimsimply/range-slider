9dcde7b8bd52a90a9584a1a248e9b04b9112a1d3
SaltySaltPie <100744463+JimBuiSTS@users.noreply.github.com>
2022-04-12 10:55:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import styles from "./app.module.scss";
 function App() {
+  const genIndices = (from, to, increment) => {
+    let arr = [];
+    for (let i = 0; i <= (to - from) / increment; i++) {
+      arr.push(from + i * increment);
+    }
+    return arr;
+  };
+  const values = genIndices(0, 100, 1);
+
+  // const [value, setValue] = useState(0);
+  const [index, setIndex] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={`${styles.contentC}`}>
+      <div className={`${styles.trackC}`}>
+        <div className={`${styles.track}`} style={{ width: `${index}%` }}></div>
+      </div>
+      <input
+        className={`${styles.input}`}
+        type="range"
+        min={0}
+        max={100}
+        value={index}
+        onChange={(e) => setIndex(e.target.value)}
+      />
+      <div className={`${styles.tooltipC}`}>
+        <div
+          className={`${styles.arrow}`}
+          style={{
+            left: `calc(${(index * 100) / (values.length - 1)}% - 4px)`,
+          }}
+        ></div>
+        <div
+          className={`${styles.tooltip}`}
+          style={{
+            left: `calc(${(index * 100) / (values.length - 1)}% - 28px)`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            className={`${styles.valC}`}
+            style={{
+              left: `calc(50% - 15px - (24px + 4px) * ${Math.floor(index)})`,
+            }}
+          >
+            {values.map((val) => (
+              <div key={val}>{val}</div>
+            ))}
+          </div>
+          <div className={`${styles.fadeLeft}`}></div>
+          <div className={`${styles.fadeRight}`}></div>
+        </div>
+      </div>
     </div>
   );
 }
